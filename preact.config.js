8aad34512064210adb12c12484abff3a25c0806c
Tobias Lundin <tobias.lundin@gmail.com>
2021-06-18 23:22:39 +0200

    add .env.local

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,9 @@
 import webpack from 'webpack';
 import dotenv from 'dotenv';
-const { parsed = {} } = dotenv.config();
+
+const config = dotenv.config();
+const localConfig = dotenv.config({ path: './.env.local' });
+const parsed = { ...config.parsed, ...localConfig.parsed };
 Object.keys(parsed).forEach(k => ( parsed[k] = JSON.stringify(parsed[k]) ));
 
 module.exports = function (config) {
