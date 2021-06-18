f2aa55d8f0a61fc8d241773f975b6d208edeb10c
Tobias Lundin <tobias.lundin@gmail.com>
2021-06-18 23:22:57 +0200

    add colors to logs

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -1,11 +1,19 @@
 import loglevel from 'loglevel';
 
-loglevel.setDefaultLevel(loglevel.levels.INFO);
+loglevel.setDefaultLevel(loglevel.levels.DEBUG);
 
-export const getLogger = (name: string): Pick<typeof loglevel, 'info' | 'error'> => {
-  const label = `${name}:`;
+let counter = 0;
+const colors: ReadonlyArray<string> = ['hotpink', 'aqua', 'aquamarine', 'fuchsia', 'ghostwhite', 'lavenderblush', 'yellow', 'tomato', 'salmon', 'powderblue', 'palegreen'];
+const getColor = () => colors[counter++ % colors.length] as string;
+const colorMap = new Map<string, string>();
+
+export const getLogger = (name: string): Pick<typeof loglevel, 'info' | 'error' | 'debug'> => {
+  const clr = colorMap.has(name) ? colorMap.get(name) : colorMap.set(name, getColor()).get(name);
+  const label = `%c[${name}]`;
+  const style = `color: ${clr}; background: black; padding: 2px`;
   return {
-    info: (...msg: any[]) => loglevel.info(label, ...msg),
-    error: (...msg: any[]) => loglevel.error(label, ...msg),
+    info: loglevel.info.bind(loglevel, label, style),
+    error: loglevel.error.bind(loglevel, label, style),
+    debug: loglevel.debug.bind(loglevel, label, style),
   }
 }
