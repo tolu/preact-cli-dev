f71a5cc5c37cf77bad9b3a249c1a5f2d3ce7a26e
Tobias Lundin <tobias.lundin@gmail.com>
2021-06-22 08:42:33 +0200

    fix dev certificates for service-worker

--- preact.config.js
+++ preact.config.js
@@ -1,15 +1,24 @@
 import webpack from 'webpack';
 import dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 const config = dotenv.config();
 const localConfig = dotenv.config({ path: './.env.local' });
 const parsed = { ...config.parsed, ...localConfig.parsed };
 Object.keys(parsed).forEach(k => ( parsed[k] = JSON.stringify(parsed[k]) ));
 
-module.exports = function (config) {
+module.exports = function (/** @type {import('webpack').Configuration} */config) {
   config.plugins.push(
     new webpack.DefinePlugin({
       process: { env: parsed },
     })
   );
+  Object.assign(config.devServer, {
+    http2: true,
+    https: {
+      key: readFileSync(path.join(__dirname, '/certs/key.pem')),
+      cert: readFileSync(path.join(__dirname, '/certs/cert.pem')),
+    },
+  });
 }
