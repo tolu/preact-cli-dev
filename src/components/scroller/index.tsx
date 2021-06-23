ae8b5c103429561bf8cb944084d38b3f79475451
Tobias Lundin <tobias.lundin@gmail.com>
2021-06-23 13:27:11 +0200

    wip keyboard nav in scroller

--- src/components/scroller/index.tsx
+++ src/components/scroller/index.tsx
@@ -3,11 +3,14 @@ import { SwimlaneBase } from '../../api/usePage';
 import { useSwimlane, SwimlaneItem } from '../../api/useSwimlane';
 import style from './style.css';
 import utils from './../utils.css';
+import { useCallback } from 'preact/hooks';
 
 const join = (...args: string[]) => args.join(' ');
 
 export const Scroller: FunctionalComponent<{ swimlane: SwimlaneBase }> = ({ swimlane }) => {
     const { swimlaneItems = [], error: swimlaneError } = useSwimlane(swimlane);
+    const handleKeyboard = useCallback(ulKeyDownHandler, []);
+    const handleFocus = useCallback(ulFocusHandler, []);
 
     if (swimlaneError) {
         return (<section>
@@ -18,7 +21,7 @@ export const Scroller: FunctionalComponent<{ swimlane: SwimlaneBase }> = ({ swim
     return (
         <section class={join(style['scroller-container'], utils['full-width'])}>
             <h2>{swimlane.name}: {swimlaneItems.length}</h2>
-            <ul class={style.scroller} role="list">
+            <ul class={style.scroller} role="list" onKeyDown={handleKeyboard} onFocus={handleFocus}>
                 {swimlaneItems.map(i => <ScrollerItem item={i} key={i.id} />)}
             </ul>
         </section>
@@ -41,3 +44,44 @@ const ScrollerItem: FunctionalComponent<{ item: SwimlaneItem }> = ({ item }) => 
         </li>
     )
 }
+
+/* extend global to store state of current focused element in ul-element */
+declare global {
+    interface HTMLUListElement {
+        focusedChild?: HTMLAnchorElement;
+    }
+}
+const ulKeyDownHandler = (event: KeyboardEvent) => {
+    const { target, code, altKey, shiftKey, ctrlKey, metaKey } = event;
+    // do nothing when control keys are pressed
+    if (altKey || ctrlKey || metaKey) return;
+    console.log('onKeyDown', { code, target })
+    if (target instanceof HTMLAnchorElement) {
+        const scroller = target.closest(`.${style.scroller}`) as HTMLUListElement | null;
+        if (!scroller) return;
+        if (/ArrowLeft|ArrowRight/i.test(code) && !shiftKey) {
+            const move = code === 'ArrowLeft' ? -1 : 1;
+            const children = Array.from(scroller.querySelectorAll('a'));
+            const nextTarget = children[children.indexOf(target) + move];
+            if (nextTarget) {
+                nextTarget.focus();
+                scroller.focusedChild = nextTarget;
+            }
+        }
+        const scrollerContainer = target.closest(`.${style['scroller-container']}`);
+        if (/Tab/i.test(code) && scrollerContainer) {
+            const next = scrollerContainer[shiftKey ? 'previousElementSibling' : 'nextElementSibling']?.querySelector('ul');
+            if (next instanceof HTMLElement) {
+                event.preventDefault();
+                next.tabIndex = -1;
+                next.focus();
+            }
+        }
+    }
+};
+const ulFocusHandler = ({ target }: FocusEvent) => {
+    if (target instanceof HTMLUListElement) {
+        const focusTarget = target.focusedChild || target.querySelector('a');
+        focusTarget && focusTarget.focus();
+    }
+};
