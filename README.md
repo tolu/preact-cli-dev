a0c706e4581296337c7646f3c957b7ce05207888
Tobias Lundin <tobias.lundin@gmail.com>
2021-06-23 12:01:02 +0200

    extract css utils

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # video-app-preact
 
-### Testing prod-build on https://localhost
+## Intro
+
+### Testing prod-build on <https://localhost>
 
 1. Install [mkcert](https://github.com/FiloSottile/mkcert)
 2. Generate certificates with `npm run gen-certs`
 3. Run `npm run serve-https`
 
+### Things learned
+
+CSS modules are only watched in `/components` so any attempt to put them outside need configuration changes to work.
 
 ### ToDo
 
@@ -15,17 +20,17 @@
 
 ## CLI Commands
 
-* `npm install`: Installs dependencies
+- `npm install`: Installs dependencies
 
-* `npm run dev`: Run a development, HMR server
+- `npm run dev`: Run a development, HMR server
 
-* `npm run serve`: Run a production-like server
+- `npm run serve`: Run a production-like server
 
-* `npm run build`: Production-ready build
+- `npm run build`: Production-ready build
 
-* `npm run lint`: Pass TypeScript files using ESLint
+- `npm run lint`: Pass TypeScript files using ESLint
 
-* `npm run test`: Run Jest and Enzyme with
+- `npm run test`: Run Jest and Enzyme with
   [`enzyme-adapter-preact-pure`](https://github.com/preactjs/enzyme-adapter-preact-pure) for
   your tests
 
--- src/components/scroller/index.tsx
+++ src/components/scroller/index.tsx
@@ -2,39 +2,42 @@ import { FunctionalComponent, h } from 'preact';
 import { SwimlaneBase } from '../../api/usePage';
 import { useSwimlane, SwimlaneItem } from '../../api/useSwimlane';
 import style from './style.css';
+import utils from './../utils.css';
 
-export const Scroller: FunctionalComponent<{ swimlane: SwimlaneBase}> = ({swimlane}) => {
-  const { swimlaneItems = [], error: swimlaneError } = useSwimlane(swimlane);
+const join = (...args: string[]) => args.join(' ');
 
-  if (swimlaneError) {
-      return (<section>
-          <p>{swimlaneError}</p>
-      </section>);
-  }
+export const Scroller: FunctionalComponent<{ swimlane: SwimlaneBase }> = ({ swimlane }) => {
+    const { swimlaneItems = [], error: swimlaneError } = useSwimlane(swimlane);
 
-  return (
-      <section class={ style['full-width'] }>
-          <h2>{swimlane.name}: {swimlaneItems.length}</h2>
-          <ul class={style.scroller} role="list">
-              { swimlaneItems.map(i => <ScrollerItem item={i} key={i.id} />) }
-          </ul>
-      </section>
-  );
+    if (swimlaneError) {
+        return (<section>
+            <p>{swimlaneError}</p>
+        </section>);
+    }
+
+    return (
+        <section class={join(style['scroller-container'], utils['full-width'])}>
+            <h2>{swimlane.name}: {swimlaneItems.length}</h2>
+            <ul class={style.scroller} role="list">
+                {swimlaneItems.map(i => <ScrollerItem item={i} key={i.id} />)}
+            </ul>
+        </section>
+    );
 };
 
-const ScrollerItem: FunctionalComponent<{item: SwimlaneItem}> = ({item}) => {
-  const link = `/watch/${item.seriesId ?? item.id}`;
-  const img = item.imagePackUri || item.originChannel._links.placeholderImage.href;
-  return (
-      <li key={item.id} class={ style['scroller-item'] }>
-          <a href={link}>
-              <figure>
-                  <picture>
-                      <img src={img} loading="lazy" />
-                  </picture>
-                  <figcaption>{item.name}</figcaption>
-              </figure>
-          </a>
-      </li>
-  )
+const ScrollerItem: FunctionalComponent<{ item: SwimlaneItem }> = ({ item }) => {
+    const link = `/watch/${item.seriesId ?? item.id}`;
+    const img = item.imagePackUri || item.originChannel._links.placeholderImage.href;
+    return (
+        <li key={item.id} class={style['scroller-item']}>
+            <a href={link}>
+                <figure>
+                    <picture>
+                        <img src={img} loading="lazy" />
+                    </picture>
+                    <figcaption>{item.name}</figcaption>
+                </figure>
+            </a>
+        </li>
+    )
 }
